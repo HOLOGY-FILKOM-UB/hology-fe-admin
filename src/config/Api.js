52d3a85a8e0a1c01259bd35ec7340932e8c1dd58
Muhammad Fachry Noorchoolish Arif <59736288+Croazt@users.noreply.github.com>
2021-08-30 19:37:48 +0700

    Update Api.js

--- src/config/Api.js
+++ src/config/Api.js
@@ -4,7 +4,6 @@ let parsedJsonData = localStorage.getItem("h0_ni128ehiond1289n");
 export default axios.create({
   baseURL:"https://api.hology.my.id/",
   headers:{
-    "Access-Control-Allow-Origin" : "*",
     "Authorization" : "Bearer "+parsedJsonData
   }
 })
