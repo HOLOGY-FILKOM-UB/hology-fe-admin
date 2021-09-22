9d7649adce1417147bc58c81306074d46c588f29
Croazt <fachry.croazt@gmail.com>
2021-09-22 17:31:00 +0700

    done

--- src/pages/capture/Capture.js
+++ src/pages/capture/Capture.js
@@ -18,6 +18,9 @@ const Capture = () => {
   const [token, setToken] = useState(localStorage.getItem("h0_ni128ehiond1289nsss"))
   const [decodedJwt, setdecodedJwt] = useState(jwtDecode(token))
   const [show, setShow] = useState(0)
+  const [link, setLink] = useState("")
+  const [teams_id, setTeams_id] = useState("")
+  const [propos, setPropos] = useState("")
   const [role, setRole] = useState(JSON.stringify(decodedJwt.admin.admin_role))
   let parsedJsonData = localStorage.getItem("h0_s7yf8q7g398fh924sss");
   const history = useHistory()
@@ -80,15 +83,28 @@ const Capture = () => {
       datas = datas + 1
       if (result[i].team_id) {
         var sata = await CollectTeamData(result[i].team_id)
+        var dsata = await getSubmission(result[i].team_id)
+        
         result[i].team_member = sata.data.message
+        result[i].team_submiss = dsata.data.message[0] ? dsata.data.message[0] : ""
         rows.push(result[i])
       }
     }
+    console.log(rows)
     setResults(rows)
   }
 
+  const getSubmission = (id) => {
+    return Api.get("/api/submissions/admin/uiux/getuploadedfile/" + id,
+      {
+        headers: {
+          "Authorization": "Bearer " + token
+        }
+      }).then(res => { return res }).then(res => { return res })
+  }
+
   const returntable = (role) => {
-    if(show === 0 || show === 2){
+    if (show === 0 || show === 2) {
       return (
         <Table>
           <tr>
@@ -102,7 +118,7 @@ const Capture = () => {
             <th>SUBMISSION</th>
           </tr>
           {compShow()}
-  
+
         </Table>
       )
     }
@@ -122,18 +138,17 @@ const Capture = () => {
       </Table>
     )
   }
-
   const compShow = () => {
 
     return (results.map((item, index) => {
       let verifColor = item.team_status === 0 ? 'red' : 'green'
       let verif = role === '"INTI"' ? <td style={{ backgroundColor: verifColor }}>
         NO ACTION ALLOWED</td> : item.team_status === 0 ?
-          <button style={{ backgroundColor: verifColor }} onClick={e => { UpdateUserStatus(1, item.team_id)}}>
-            VERIF BERKAS</button> :
-            <div style={{ backgroundColor: verifColor }}>
-              <button onClick={e => { UpdateUserStatus(0, item.team_id) }}>UNVERIF</button></div> 
-      if (show === 0 && item.competition_id !=2) {
+        <button style={{ backgroundColor: verifColor }} onClick={e => { UpdateUserStatus(1, item.team_id) }}>
+          VERIF BERKAS</button> :
+        <div style={{ backgroundColor: verifColor }}>
+          <button onClick={e => { UpdateUserStatus(0, item.team_id) }}>UNVERIF</button></div>
+      if (show === 0 && item.competition_id != 2) {
         return (
           <tr key={index}>
             <th width="3%">{item.team_id}</th>
@@ -167,7 +182,7 @@ const Capture = () => {
               <th width="15%">{item.team_name}</th>
               <th width="5%">{item.competition_id === 1 ? "CP" : item.competition_id === 2 ? "UI/UX" : item.competition_id === 3 ? "CTF" : "CTF SMA"}</th>
               <th width="15%">{item.institution_name}</th>
-            <th width="15%">{ProofDetector("bayar", item.team_payment_proof)}</th>
+              <th width="15%">{ProofDetector("bayar", item.team_payment_proof)}</th>
               <th>
                 <Tables width="100%">
                   {item.team_member.map((anggota, index) => {
@@ -207,10 +222,13 @@ const Capture = () => {
                   })}
                 </Tables>
               </th>
-              <th>No Action Allowed</th>
+              <th>
+                {item.team_submiss === "" ? <p>TIM BELUM SUBMIT</p> : ProofDetector2("link", item.team_submiss.submission_link,  item.team_submiss.submission_file)}
+ 
+              </th>
             </tr>
           )
-        }else if (item.competition_id === show) {
+        } else if (item.competition_id === show) {
           return (
             <tr key={index}>
               <th width="3%">{item.team_id}</th>
@@ -241,8 +259,8 @@ const Capture = () => {
 
 
   const UpdateUserStatus = (status, team_id) => {
-    
-    Api.put("/api/teams/" +status + "/" + team_id, {},{
+
+    Api.put("/api/teams/" + status + "/" + team_id, {}, {
       headers: {
         "Authorization": "Bearer " + token
       }
@@ -273,6 +291,40 @@ const Capture = () => {
     setShow(competition_id)
   }
 
+  const ProofDetector2 = (name, type1, type2) => {
+
+    if (type1 && type2) {
+      return (
+        <div>
+          <button onClick={e => { window.open(type1) }}>LINK</button>
+          <button onClick={e => { window.open("https://api.hology.my.id/payment/" + type2) }}>PROPOSAL</button>
+        </div>
+      )
+
+    }
+    if (type1) {
+      return (
+        <div>
+          <button onClick={e => { window.open(type1) }}>LINK</button>
+          <p>TIDAK ADA PROP</p>
+        </div>
+      )
+
+    }
+    if (type2) {
+      return (
+        <div>
+          <p>TIDAK ADA LINK</p>
+          <button onClick={e => { window.open("https://api.hology.my.id/payment/" + type2) }}>PROPOSAL</button>
+        </div>
+      )
+
+    }
+    return (
+      <p>SUBMISS TIDAK ADA</p>
+    )
+
+  }
   const ProofDetector = (name, type) => {
     if (name === "ktm") {
       if (type) {
